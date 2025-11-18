edae3d6db1f5ceea83ee7c835e2436cc15068f83
Sri Sakthi <95374074+Sri-Sakthi-CB@users.noreply.github.com>
2025-11-18 22:48:39 +0530

    Add comment on tight coupling in TightCoupling.java
    Added a comment explaining tight coupling in the code.

--- TightCoupling/TightCoupling.java
+++ TightCoupling/TightCoupling.java
@@ -1,5 +1,7 @@
 package org.example.TightCoupling;
 
+//Tight Coupling - one class is tightly linked to another class, making the code difficult to modify or test separately.
+
 public class TightCoupling {
     public static void main(String[] args) {
 
